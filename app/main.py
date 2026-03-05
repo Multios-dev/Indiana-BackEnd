19d01003893c313611ec9c989e4620a6c70ee085
amelbnsm <la237329@student.helha.be>
2026-03-05 09:49:37 +0100

    ajout du routeur dans le main

--- app/main.py
+++ app/main.py
@@ -1,13 +1,6 @@
 ﻿from fastapi import FastAPI
-
+from app.api.routes.auth import router as auth_router
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+app.include_router(auth_router)
