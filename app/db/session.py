6d6cc209f1227be5d37ce3de10bd0cc485ab01a0
amelbnsm <la237329@student.helha.be>
2026-03-05 15:44:18 +0100

    Ajout de la connexion à la db (en local pour l'instant)

--- app/db/session.py
+++ app/db/session.py
@@ -1,1 +1,27 @@
-# à faire quand la db sera créée
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+
+DATABASE_URL = "postgresql+asyncpg://postgres:admin123@localhost:5432/indiana"
+
+# engine est un objet qui gère la connexion à la base de données
+# il peut ouvrir des connexions et envoyer des requêtes sql
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=True, # affiche les requêtes sql dans le terminal
+)
+
+# SessionLocal fabrique des sessions
+# chaque requête API aura sa propre session
+SessionLocal = async_sessionmaker(
+    bind=engine,
+    expire_on_commit=False,
+)
+
+# Fonctione utilisée pour obtenir une session DB
+# FastAPI l'utilisera avec Depends(get_db)
+async def get_db():
+    """
+    Crée une session de base de données et la ferme
+    automatiquement après la requête
+    """
+    async with SessionLocal() as session:
+        yield session
