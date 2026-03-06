706e3a985da40da2553b92d768f147f7520a8cb6
amelbnsm <la237329@student.helha.be>
2026-03-06 14:27:55 +0100

    Changement du format retourné par les routes (dto)

--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db.repositories.user.user_repository import UserRepository
 from app.db.session import get_db
+from app.schemas.dtos.output.get_user_output import GetUserOutput
 from app.services.user_service import UserService
 
 router = APIRouter(prefix="/users", tags=["users"])
@@ -11,10 +12,50 @@ router = APIRouter(prefix="/users", tags=["users"])
 async def get_users(db:AsyncSession = Depends(get_db)):
     repo = UserRepository(db)
     service = UserService(repo)
-    return await service.get_all_users()
 
-@router.get("/{id}", summary="Récupérer un utilisateur spécifique")
+    try:
+        users = await service.get_all_users()
+
+        # Retourne tous les utilisateurs sous forme de liste
+        return [
+            GetUserOutput(
+                id=user.id,
+                firstNames=user.firstNames,
+                lastName=user.lastName,
+                birthDate=user.birthDate,
+                gender=user.gender,
+                nationality=user.nationality,
+                street=user.street,
+                zip=user.zip,
+                city=user.city,
+                email=user.email,
+                phone=user.phone
+            )
+            for user in users
+        ]
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e))
+
+@router.get("/{user_id}", summary="Récupérer un utilisateur spécifique")
 async def get_user(user_id:int,db:AsyncSession = Depends(get_db)):
     repo = UserRepository(db)
     service = UserService(repo)
-    return await service.get_user_by_id(user_id)
+
+    try:
+        user = await service.get_user_by_id(user_id)
+
+        return GetUserOutput(
+            id=user.id,
+            firstNames=user.firstNames,
+            lastName=user.lastName,
+            birthDate=user.birthDate,
+            gender=user.gender,
+            nationality=user.nationality,
+            street=user.street,
+            zip=user.zip,
+            city=user.city,
+            email=user.email,
+            phone=user.phone
+        )
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e))
