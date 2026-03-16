5bd56ee079644a84492b5ef636cb9d783681320b
amelbnsm <la237329@student.helha.be>
2026-03-16 09:58:18 +0100

    corrections

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -8,6 +8,14 @@ from app.db.models.user_model import User
 
 from datetime import date
 
+from app.core.exceptions import (
+    UserNotFoundError,
+    MembershipNotFoundError,
+    OrganizationNotFoundError,
+    InvalidDateRangeError,
+    EmptyUpdatePayloadError
+)
+
 def get_user_service(db: AsyncSession = Depends(get_db)):
     repo = UserRepository(db)
     return UserService(repo)
@@ -21,28 +29,28 @@ class UserService:
     async def get_users(self, filters:dict | None = None):
         users = await self.repo.get_users(filters)
         if not users:
-            raise ValueError("Users not found")
+            raise UserNotFoundError()
         return users
 
     # Récupérer un utilisateur spécifique
     async def get_user_by_id(self, user_id):
         user = await self.repo.get_user_by_id(user_id)
         if not user:
-            raise ValueError("User not found")
+            raise UserNotFoundError()
         return user
 
     # Modifier les données d'un utilisateur
     async def update_user(self, user_id, data:dict):
         updated_user = await self.repo.update_user(user_id, data)
         if not updated_user:
-            raise ValueError("User not found")
+            raise UserNotFoundError()
         return updated_user
 
     # Supprimer un utilisateur
     async def delete_user(self, user_id):
         deleted_user = await self.repo.delete_user(user_id)
         if not deleted_user:
-            raise ValueError("User not found")
+            UserNotFoundError()
         return deleted_user
 
     # Créer un utilisateur
