218286fcc3e74929a90dd3149ace4cb08d6c7634
amelbnsm <la237329@student.helha.be>
2026-03-24 18:48:46 +0100

    validations pour les champs mandats

--- app/schemas/dtos/input/membership_input.py
+++ app/schemas/dtos/input/membership_input.py
@@ -1,6 +1,7 @@
 from datetime import date
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator, model_validator
 from decimal import Decimal
+from typing import Self
 
 class CreateMembershipInput(BaseModel):
     user_id: int
@@ -10,8 +11,34 @@ class CreateMembershipInput(BaseModel):
     end_date: date | None = None
     price:Decimal | None = None
 
+    @field_validator("price")
+    def validate_price(cls, v):
+        if v is not None and v < 0:
+            raise ValueError("Price cannot be negative")
+        return v
+
+    # model_validator permet de valider plusieurs champs ensemble
+    # ici, on compare start_date et end_date entre eux
+    # "self" = l'objet complet avec tous ses champs accessibles via self.xxx
+    @model_validator(mode="after")
+    def validate_dates(self) -> Self:
+        if self.end_date and self.end_date < self.start_date:
+            raise ValueError("End date cannot be less than start date")
+        return self
+
 class UpdateMembershipInput(BaseModel):
     role: str | None = None
     start_date: date | None = None
     end_date: date | None = None
     price: Decimal | None = None
+
+    @field_validator("price")
+    def validate_price(cls, v):
+        if v is not None and v < 0:
+            raise ValueError("Price cannot be negative")
+        return v
+    @model_validator(mode="after")
+    def validate_dates(self) -> Self:
+        if self.end_date and self.end_date < self.start_date:
+            raise ValueError("End date cannot be less than start date")
+        return self
