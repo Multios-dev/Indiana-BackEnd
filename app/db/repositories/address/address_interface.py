520e8bfaf9e646d5ca273b80895d27b41369afa1
amelbns <la237329@student.helha.be>
2026-04-08 15:57:27 +0200

    création de la couche infra

--- app/db/repositories/address/address_interface.py
+++ app/db/repositories/address/address_interface.py
@@ -1,1 +1,10 @@
-# TODO : à faire lors de l'implémentation des adresses
+from app.db.models.address_model import Address
+from abc import ABC, abstractmethod
+
+class AddressInterface(ABC):
+    @abstractmethod
+    async def create_address(self, address: Address) -> Address: ...
+    @abstractmethod
+    async def update_address(self, addres_id:UUID, data:dict) -> Address | None: ...
+    @abstractmethod
+    async def delete_address(self, address_id:UUID) -> bool: ...
--- app/db/repositories/address/address_repository.py
+++ app/db/repositories/address/address_repository.py
@@ -1,20 +1,43 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 from app.db.models.address_model import Address
+from app.db.repositories.address.address_interface import AddressInterface
+from uuid import UUID
 
-class AddressRepository:
-    """
-    Repo pour gérer les adresses.
-    Encapsule toutes les opérations DB sur Address.
-    """
-
-    def __init__(self, session: AsyncSession):
-        self.session = session
+class AddressRepository(AddressInterface):
+    def __init__(self, db: AsyncSession):
+        self.db = db
 
     async def create_address(self, address: Address) -> Address:
-        """
-        Crée une adresse dans la DB et retourne l'entité avec ID.
-        """
-        self.session.add(address)
-        await self.session.commit()
-        await self.session.refresh(address)
+        self.db.add(address)
+        await self.db.commit()
+        await self.db.refresh(address)
         return address
+
+    async def update_address(self, address_id:UUID, data:dict) -> Address | None:
+        stmt = select(Address).where(Address.id == address_id)
+        result = await self.db.execute(stmt)
+        address = result.scalar_one_or_none()
+
+        if not address:
+            return None
+
+        for key, value in data.items():
+            if hasattr(address, key):
+                setattr(address, key, value)
+
+        await self.db.commit()
+        await self.db.refresh(address)
+        return address
+
+    async def delete_address(self, address_id: UUID) -> bool:
+        stmt = select(Address).where(Address.id == address_id)
+        result = await self.db.execute(stmt)
+        address = result.scalar_one_or_none()
+
+        if not address:
+            return None
+
+        await self.db.delete(address)
+        await self.db.commit()
+        return True
