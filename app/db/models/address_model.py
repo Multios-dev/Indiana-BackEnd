6262aaa817869583de7ea0c80ce56912e62dc121
amelbns <la237329@student.helha.be>
2026-04-08 12:07:26 +0200

    maj du modèle de données des adresses

--- app/db/models/address_model.py
+++ app/db/models/address_model.py
@@ -6,8 +6,9 @@ from app.db.base import Base
 class Address(Base):
     __tablename__ = "addresses"
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    thoroughfare = Column(String, nullable=False)  # rue + numéro
     box_number = Column(String, nullable=False)
-    street = Column(String, nullable=False)
     post_name = Column(String, nullable=False)
     post_code = Column(String, nullable=False)
     country = Column(String, nullable=False)
+    # add the BeSt id later
