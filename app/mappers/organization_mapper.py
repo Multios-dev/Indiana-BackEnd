83ccbe72bd507330f9c91a7c7cf98b685c30c629
amelbns <la237329@student.helha.be>
2026-04-09 14:23:10 +0200

    ajout de l'adresse en db lors de la création d'une organisation

--- app/mappers/organization_mapper.py
+++ app/mappers/organization_mapper.py
@@ -1,6 +1,7 @@
 from app.db.models.contact_model import Contact
 from app.schemas.dtos.input.organization_input import CreateOrganizationInput
 from app.db.models.organization_model import Organization
+from app.db.models.address_model import Address
 
 class OrganizationMapper:
     @staticmethod
@@ -11,8 +12,13 @@ class OrganizationMapper:
         return Organization(**data)
 
     @staticmethod
-    def to_contact_entity(payload:CreateOrganizationInput, org_id:int) -> Contact:
+    def to_contact_entity(payload:CreateOrganizationInput, org_id:int) -> Contact | None:
         if not payload.contact:
             return None
         data = payload.contact.model_dump(exclude_none=True)
         return Contact(org_id=org_id, **data)
+
+    @staticmethod
+    def to_address_entity(address_payload) -> Address | None:
+        data = address_payload.model_dump(exclude_none=True)
+        return Address(**data)
