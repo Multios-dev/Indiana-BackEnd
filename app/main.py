03137a58bbe99c402e5b50423fe5c28cac5c8e24
amelbns <la237329@student.helha.be>
2026-04-10 14:32:10 +0200

    ajout endpoint pour récupérer les adresses

--- app/main.py
+++ app/main.py
@@ -1,16 +1,13 @@
-﻿from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
-import traceback
+﻿from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from app.api.routes.users import router as user_router
 from app.api.routes.organizations import router as organization_router
 from app.api.routes.memberships import router as memberships_router
 from app.api.routes.events import router as events_router
-
+from app.api.routes.addresses import router as address_router
 from app.core.exceptions import AppException
 from app.core.redis.exception_handlers import app_exception_handler
 from app.db.init_db import init_db
-from app.db.model_loader import load_all_models
 from app.db.session import engine
 from contextlib import asynccontextmanager
 
@@ -39,3 +36,4 @@ app.include_router(user_router)
 app.include_router(organization_router)
 app.include_router(memberships_router)
 app.include_router(events_router)
+app.include_router(address_router)
