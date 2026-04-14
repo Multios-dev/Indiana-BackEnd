7ce853fd3868e79638b34e8df6f2c9f3a09efdf4
amelbns <la237329@student.helha.be>
2026-04-14 02:18:23 +0200

    ajout middleware pour les exceptions

---
+++ app/middlewares/exception_middleware.py
@@ -0,0 +1,30 @@
+from fastapi import HTTPException
+from starlette.requests import Request
+from starlette.responses import JSONResponse
+
+def add_cors_headers(response: JSONResponse, origin: str | None) -> JSONResponse:
+    # Add CORS headers to the response if an origin is present
+    if origin:
+        response.headers["Access-Control-Allow-Origin"] = origin
+        response.headers["Access-Control-Allow-Credentials"] = "true"
+        response.headers["Vary"] = "Origin"
+    return response
+
+async def exception_middleware(request: Request, call_next):
+    try:
+        response = await call_next(request)
+        return response
+    except HTTPException as e:
+        # Handle known HTTP exceptions (401, 403, 404, etc.)
+        resp = JSONResponse(
+            status_code=e.status_code,
+            content={"detail": e.detail}
+        )
+        return add_cors_headers(resp, request.headers.get("origin"))
+    except Exception as e:
+        # Handle unexpected server errors
+        resp =  JSONResponse(
+            status_code=500,
+            content={"detail": f"Unexpected server error: {str(e)}"}
+        )
+        return add_cors_headers(resp, request.headers.get("origin"))
