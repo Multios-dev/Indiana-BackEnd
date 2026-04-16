e06821681f65bfd23afa9b553382fa0e72538634
amelbnsm <la237329@student.helha.be>
2026-04-16 09:58:23 +0200

    ajout de la nationalité dans les dtos

--- app/schemas/dtos/input/user_input.py
+++ app/schemas/dtos/input/user_input.py
@@ -29,6 +29,7 @@ class UserCreateInput(BaseModel):
     last_name: str | None = None
     birth_date: date | None = None
     gender: str | None = None
+    nationality: str | None = None
     totem: str | None = None
     quali: str | None = None
     is_legal_guardian: bool = False
@@ -48,6 +49,7 @@ class UserUpdateInput(BaseModel):
     last_name: str | None = None
     birth_date: date | None = None
     gender: str | None = None
+    nationality: str | None = None
     totem: str | None = None
     quali: str | None = None
     is_legal_guardian: bool | None = None
