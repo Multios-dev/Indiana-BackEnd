e6f7e296df1741cbe8370718010771aed8901dcb
amelbnsm <la237329@student.helha.be>
2026-04-16 11:34:22 +0200

    correction du service

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -4,6 +4,8 @@ from app.db.models.user_model import User
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import Depends
 from app.db.session import get_db
+from app.schemas.dtos.input.user_input import UserLoginInput
+
 
 def get_auth_service(db: AsyncSession = Depends(get_db)):
     repo = AuthRepository(db)
@@ -13,12 +15,11 @@ class AuthService:
     def __init__(self, repo:AuthRepository):
         self.repo = repo
 
-    async def login(self, email: str, password: str) -> User:
-        user = await self.repo.get_user_by_email(email)
+    async def login(self, payload: UserLoginInput) -> User:
+        user = await self.repo.get_user_by_email(payload.email)
         if not user:
             raise UserNotFoundError()
 
-        if password != user.password:
+        if payload.password != user.password:
             raise PasswordError()
-
         return user
