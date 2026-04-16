19aa1b558a5f4dc2818acc6e66fb36cf708965ae
amelbnsm <la237329@student.helha.be>
2026-04-16 11:38:50 +0200

    maj routeur

--- app/api/routes/auth.py
+++ app/api/routes/auth.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, Depends
 from app.schemas.dtos.input.user_input import UserLoginInput
+from app.schemas.dtos.output.user_output import UserOutput, UserLoginOutput
 from app.services.auth_service import AuthService, get_auth_service
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
-@router.post("/login")
+@router.post("/login", response_model=UserLoginOutput)
 async def login(
         payload: UserLoginInput,
         service: AuthService = Depends(get_auth_service),
