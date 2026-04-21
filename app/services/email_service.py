b74f62e142779831d70e5bca8029eaba0739507d
amelbnsm <la237329@student.helha.be>
2026-04-21 12:22:21 +0200

    ajout de la route

--- app/services/email_service.py
+++ app/services/email_service.py
@@ -5,12 +5,16 @@ from jinja2 import Environment, FileSystemLoader
 resend.api_key = settings.RESEND_API_KEY
 templates = Environment(loader=FileSystemLoader('templates'))
 
-async def send_registration_email(self, to:str, first_name:str) -> None:
-    html = templates.get_template("registration.html").render(first_name=first_name)
-    params: resend.Emails.SendParams = {
-        "from" : settings.MAIL_FROM,
-        "to" : [to],
-        "subject" : "Bienvenue sur Indiana !",
-        "html" : html
-    }
-    await resend.Emails.send_async(params)
+def get_email_service():
+    return EmailService()
+
+class EmailService:
+    async def send_registration_email(self, to:str, first_name:str) -> None:
+        html = templates.get_template("registration.html").render(first_name=first_name)
+        params: resend.Emails.SendParams = {
+            "from" : settings.MAIL_FROM,
+            "to" : [to],
+            "subject" : "Bienvenue sur Indiana !",
+            "html" : html
+        }
+        await resend.Emails.send_async(params)
