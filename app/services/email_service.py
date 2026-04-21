f2255108f5fdd4c6e39871bac7f7b2a93cc4d0b5
amelbnsm <la237329@student.helha.be>
2026-04-21 12:14:35 +0200

    implémentation du service pour l'envoi de mails

--- app/services/email_service.py
+++ app/services/email_service.py
@@ -1,4 +1,16 @@
 import resend
 from app.core.config import settings
+from jinja2 import Environment, FileSystemLoader
 
 resend.api_key = settings.RESEND_API_KEY
+templates = Environment(loader=FileSystemLoader('templates'))
+
+async def send_registration_email(self, to:str, first_name:str) -> None:
+    html = templates.get_template("registration.html").render(first_name=first_name)
+    params: resend.Emails.SendParams = {
+        "from" : settings.MAIL_FROM,
+        "to" : [to],
+        "subject" : "Bienvenue sur Indiana !",
+        "html" : html
+    }
+    await resend.Emails.send_async(params)
