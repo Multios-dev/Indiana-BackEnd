a51baa69869620dab736e4af3a4739e1f9cb0a5b
amelbnsm <la237329@student.helha.be>
2026-04-21 12:36:58 +0200

    utilisation de google smtp

--- app/services/email_service.py
+++ app/services/email_service.py
@@ -1,9 +1,10 @@
-import resend
+import aiosmtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from app.core.config import settings
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from pathlib import Path
 
-resend.api_key = settings.RESEND_API_KEY
 templates = Environment(
     loader=FileSystemLoader(Path(__file__).parent.parent / "templates"),
     autoescape=select_autoescape(["html"]),
@@ -13,12 +14,20 @@ def get_email_service():
     return EmailService()
 
 class EmailService:
-    async def send_registration_email(self, to:str, first_name:str) -> None:
+    async def send_registration_email(self, to: str, first_name: str) -> None:
         html = templates.get_template("registration_template.html").render(first_name=first_name)
-        params: resend.Emails.SendParams = {
-            "from" : settings.MAIL_FROM,
-            "to" : [to],
-            "subject" : "Bienvenue sur Indiana !",
-            "html" : html
-        }
-        await resend.Emails.send_async(params)
+
+        message = MIMEMultipart("alternative")
+        message["From"] = settings.MAIL_FROM
+        message["To"] = to
+        message["Subject"] = "Bienvenue sur Indiana !"
+        message.attach(MIMEText(html, "html"))
+
+        await aiosmtplib.send(
+            message,
+            hostname="smtp.gmail.com",
+            port=587,
+            username=settings.MAIL_FROM,
+            password=settings.GMAIL_APP_PASSWORD,
+            start_tls=True,
+        )
