ff68ef4a3f8f1c9d1bb35ff0f126e7eaaab9a2da
amelbnsm <la237329@student.helha.be>
2026-04-21 12:27:02 +0200

    correction chemin

--- app/services/email_service.py
+++ app/services/email_service.py
@@ -1,16 +1,20 @@
 import resend
 from app.core.config import settings
-from jinja2 import Environment, FileSystemLoader
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from pathlib import Path
 
 resend.api_key = settings.RESEND_API_KEY
-templates = Environment(loader=FileSystemLoader('templates'))
+templates = Environment(
+    loader=FileSystemLoader(Path(__file__).parent.parent / "templates"),
+    autoescape=select_autoescape(["html"]),
+)
 
 def get_email_service():
     return EmailService()
 
 class EmailService:
     async def send_registration_email(self, to:str, first_name:str) -> None:
-        html = templates.get_template("registration.html").render(first_name=first_name)
+        html = templates.get_template("registration_template.html").render(first_name=first_name)
         params: resend.Emails.SendParams = {
             "from" : settings.MAIL_FROM,
             "to" : [to],
