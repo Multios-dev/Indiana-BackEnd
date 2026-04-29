581317a784dcad06bdb234560f9357d165902ae3
amelbns <la237329@student.helha.be>
2026-04-29 17:10:50 +0200

    correction dto

--- app/schemas/dtos/output/participation_output.py
+++ app/schemas/dtos/output/participation_output.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 from uuid import UUID
 
 class ParticipationOutput(BaseModel):
-    participation_id: UUID
+    id: UUID
     user_id: UUID
     event_id: UUID
     role:str
