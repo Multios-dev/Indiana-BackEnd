e79d2793b7b66b87cf3e6670d42933608314bee3
amelbns <la237329@student.helha.be>
2026-04-29 18:05:44 +0200

    ajout route pour récupérer toutes les participations (avec filtres)

--- app/db/repositories/participation/participation_repository.py
+++ app/db/repositories/participation/participation_repository.py
@@ -1,5 +1,6 @@
+from typing import List
 from uuid import UUID
-from sqlalchemy import select, update
+from sqlalchemy import select, update, and_
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.models.participation_model import Participation
 from app.db.repositories.participation.participation_interface import ParticipationInterface
@@ -38,3 +39,24 @@ class ParticipationRepository(ParticipationInterface):
         result = await self.db.execute(stmt)
         await self.db.commit()
         return result.scalar_one_or_none()
+
+    async def get_all_participations(self, filters:dict | None = None)->List[Participation]:
+        stmt = select(Participation)
+
+        conditions = []
+
+        if filters:
+            allowed_filters = {
+                "event_id",
+                "user_id"
+            }
+
+            for key, value in filters.items():
+                if key not in allowed_filters and hasattr(Participation, key):
+                    conditions.append(getattr(Participation, key) == value)
+
+        if conditions:
+            stmt = stmt.where(and_(*conditions))
+
+        result = await self.db.execute(stmt)
+        return result.scalars().all()
