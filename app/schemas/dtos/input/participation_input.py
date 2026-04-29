476ae7f9175a4b24b037f17292fc4437347df71a
amelbns <la237329@student.helha.be>
2026-04-29 15:06:25 +0200

    dto pour la modif d'une participation

--- app/schemas/dtos/input/participation_input.py
+++ app/schemas/dtos/input/participation_input.py
@@ -14,3 +14,15 @@ class ParticipationInvitationInput(BaseModel):
         if v is not None and v < 0:
             raise ValueError("Price cannot be negative")
         return v
+
+class ParticipationUpdateInput(BaseModel):
+    user_id:UUID | None = None
+    event_id:UUID | None = None
+    role:str | None = None
+    price:Decimal | None = None
+
+    @field_validator("price")
+    def validate_price(cls, v):
+        if v is not None and v < 0:
+            raise ValueError("Price cannot be negative")
+        return v
